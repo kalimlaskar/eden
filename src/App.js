8f000424d63361656e6915ea6bf8c42004b6c1f0
kalimlaskar <kalim007mailbox@gmail.com>
2022-08-16 18:59:18 +0530

    changes hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Welcome from './components/Welcome';
 import Homework from './components/Homework';
 import Planning from './components/Planning';
